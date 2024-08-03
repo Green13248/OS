5d4ed02c01171ee068a1cc6eec3a747f3b7faf50
Green13248 <149907310+Green13248@users.noreply.github.com>
2024-08-02 21:30:53 -0400

    Update HBoot.asm

--- HBoot.asm
+++ HBoot.asm
@@ -1,17 +1,36 @@
-.text            
-.global _start
-_start:
-    mov r0, #1
-    ldr r1, =message
-    ldr r2, =len
-    mov r7, #4
-    swi 0
+[ORG 0x7c00]
+bits 16
 
-    mov r7, #1
-    swi 0
+mov si, 0
 
-.data
-message:
-    .asciz "hello world\n"
-len = .-message                  
-            
+call cls
+jmp $
+
+cls:
+    pusha
+    mov ah, 0x00
+    mov al, 0x03  ; text mode 80x25 16 colors
+    int 0x10
+    popa
+
+print:
+    mov ah, 0x0e
+    mov al, [info + si]
+    int 0x10
+    add si, 1
+    cmp byte [info + si], 0
+    jne print
+
+prompt:
+    mov ah, 0x0e
+    mov al, ">"
+    int 0x10
+jmp $
+
+DiskNum: db 0
+
+info:
+    db "Welcome to Ben's OS",13,10, 0
+
+times 510-($-$$) db 0	; Pad the bootloader to 510 bytes
+dw 0xAA55			   ; Boot signature at 511-512 bytes
