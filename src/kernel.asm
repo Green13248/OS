6267637d666301fa6ecb068ced1c6981433966ca
Green13248 <149907310+Green13248@users.noreply.github.com>
2024-09-08 12:42:32 -0400

    Update kernel.asm

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,1 +1,18 @@
-
+cls:	
+	mov ah, 0x00
+	mov al, 0x03
+	int 0x10
+	jmp Loop
+Loop:	
+	mov ah, 0x0e
+	mov al, 10
+	int 0x10
+	mov al, 13
+	int 0x10
+	mov al, ">"
+	int 0x10
+	mov ah, 0
+	int 0x16
+	cmp al, "C"
+	je cls
+	jmp Loop
